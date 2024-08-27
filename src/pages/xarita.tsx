c639e2f5bfd2cfb9f549e49e9a62b00057d94708
Navruza21 <navruzaxusniddinovna@gmail.com>
2024-08-27 15:11:59 +0500

    edit all xarita n

--- src/pages/xarita.tsx
+++ src/pages/xarita.tsx
@@ -22,7 +22,12 @@ export const Xarita = () => {
           padding: "0",
         }}
       ></div>
-      <YMaps>
+      <YMaps
+        query={{
+          ns: "use-load-option",
+          load: "Map,Placemark,control.ZoomControl,control.FullscreenControl,geoObject.addon.balloon",
+        }}
+      >
         <Map
           defaultState={{ center: [41.327169, 69.282666], zoom: 13 }}
           className="w-[1050px] h-[550px] m-10 border-solid border-8 border-white mb-10"
@@ -31,42 +36,43 @@ export const Xarita = () => {
           {/* <TypeSelector options={{ float: "left" }} /> */}
 
           <Placemark
-            // onChange={() => {
-            //   console.log("shaxriston");
-            // }}
             geometry={[41.353122, 69.289043]}
             properties={{
-              balloonContentHeader: "Оператор: Иван Иванов",
-              balloonContentBody: "Телефон: +998 90 123 45 67",
+              balloonContentHeader: "Operator: Nazarova Munisa",
+              balloonContentBody: "Telefon: +998 90 123 45 67",
             }}
           />
           {/* shaxriston */}
           <Placemark
-            onChange={() => {
-              console.log("xamid olimjon");
-            }}
             geometry={[41.318295, 69.294966]}
+            properties={{
+              balloonContentHeader: "Operator: Hamidova Saida",
+              balloonContentBody: "Telefon: +998 90 111 44 66",
+            }}
           />
           {/* xamid olimjon */}
           <Placemark
-            onChange={() => {
-              console.log("tinchlik");
-            }}
             geometry={[41.333155, 69.219448]}
+            properties={{
+              balloonContentHeader: "Operator: Karimova Nigora",
+              balloonContentBody: "Telefon: +998 90 333 55 77",
+            }}
           />
           {/* tinchlik */}
           <Placemark
-            onChange={() => {
-              console.log("alisher navoiy");
-            }}
             geometry={[41.319601, 69.254077]}
+            properties={{
+              balloonContentHeader: "Operator: Sadriddinov Farrux",
+              balloonContentBody: "Telefon: +998 90 222 11 55",
+            }}
           />
           {/* alisher navoiy */}
           <Placemark
-            onChange={() => {
-              console.log("ming orik");
-            }}
             geometry={[41.299734, 69.273612]}
+            properties={{
+              balloonContentHeader: "Operator: Suvonov Mansur",
+              balloonContentBody: "Telefon: +998 90 333 44 55",
+            }}
           />
           {/* ming orik */}
         </Map>
