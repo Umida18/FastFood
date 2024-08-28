f738d8065851c89a77766c3a7f7b23bfa70449f7
umida18 <https://github.com/Navruza21/school.git>
2024-08-28 11:38:43 +0400

    edit u

--- src/pages/buyurtmalar/buyurmalar.tsx
+++ src/pages/buyurtmalar/buyurmalar.tsx
@@ -1,48 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { Typography, Drawer, Button, Avatar, Tabs } from "antd";
-import { FaPlus } from "react-icons/fa6";
-import { IoCheckmark } from "react-icons/io5";
+import { FaPlus, FaRegUser } from "react-icons/fa6";
+import { IoCheckmark, IoClipboardOutline } from "react-icons/io5";
 import { TbX } from "react-icons/tb";
 import TabPane from "antd/es/tabs/TabPane";
+import axios from "axios";
+import { CiBookmark, CiDeliveryTruck } from "react-icons/ci";
+import { MdOutlineAccessTime } from "react-icons/md";
+import { FiPhone } from "react-icons/fi";
+import { GoColumns } from "react-icons/go";
+import { PiRowsThin } from "react-icons/pi";
+
 interface UserInfo {
-  name: string;
+  first_name: string;
+  last_name: string;
   phone_number: string;
 }
-
-interface TraDetails {
-  first_amount: string;
-  second_amount: string;
+interface Payment {
+  order_amount: string;
+  delivery_amount: string;
   total_amount: string;
-  payment_method: string;
-}
-
-interface Operator {
-  name: string;
-}
-
-interface Branch {
-  name: string;
+  payment_method: number;
 }
 
 interface Order {
-  tra_id: string;
-  tra_time: string;
+  order_id: number;
+  order_time: string;
   user_info: UserInfo;
-  tra_details: TraDetails;
-  operator: Operator;
-  branch: Branch;
+  order_details: Payment;
 }
-
+interface PaymentM {
+  id: number;
+  name: string;
+}
 export const Buyurtmalar: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
-  const [data, setData] = useState<Order[]>([]);
+  const [buyurtma, setBuyurtma] = useState<Order[]>([]);
+  const [tolovTuri, setTolovTuri] = useState<PaymentM[]>([]);
   const [activeTab, setActiveTab] = useState<string>("yangi");
   const [searchTerm, setSearchTerm] = useState<string>("");
-
-  const handleTabChange = (key: string): void => {
-    setActiveTab(key);
-    // Trigger filtering or other actions based on the selected tab
-  };
+  const [selectedView, setSelectedView] = useState<string>("columns");
 
   const showDrawer = () => {
     setOpen(true);
@@ -53,13 +50,32 @@ export const Buyurtmalar: React.FC = () => {
   };
 
   useEffect(() => {
-    fetch("https://10d4bfbc5e3cc2dc.mokky.dev/buyurtma")
-      .then((response) => response.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error("Error fetching data:", error));
+    const fetchData = async () => {
+      const reponseB = await axios.get(
+        "https://10d4bfbc5e3cc2dc.mokky.dev/buyurtma"
+      );
+      const responseT = await axios.get(
+        "https://10d4bfbc5e3cc2dc.mokky.dev/paymentMethod"
+      );
+      setBuyurtma(reponseB.data);
+      setTolovTuri(responseT.data);
+    };
+    fetchData();
   }, []);
-
+  const handleTabChange = () => {};
   const { Text, Title } = Typography;
+
+  const getPaymentMethod = (paymentId: number) => {
+    const payment = tolovTuri.find((item) => item.id === paymentId);
+    return payment ? payment.name : "Unknown";
+  };
+  const PriceComponent = ({ price }: { price: string }) => {
+    const formatPrice = (price: string) => {
+      return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+
+    return <div>{formatPrice(price)} UZS</div>;
+  };
 
   return (
     <div className="">
@@ -96,100 +112,200 @@ export const Buyurtmalar: React.FC = () => {
             </Tabs>
           </div>
         </div>
+        <div className="bg-[#edeff3] rounded-[30px] flex justify-center gap-1 items-center w-[97px] h-[48px]">
+          <Button
+            onClick={() => setSelectedView("columns")}
+            style={{
+              backgroundColor: selectedView === "columns" ? "white" : "#edeff3",
+              border: "none",
+              boxShadow: "none",
+              borderRadius: "50%",
+              width: "35px",
+              height: "35px",
+              padding: "0px",
+            }}
+          >
+            <GoColumns />
+          </Button>
+          <Button
+            onClick={() => setSelectedView("rows")}
+            style={{
+              backgroundColor: selectedView === "rows" ? "white" : "#edeff3",
+              border: "none",
+              boxShadow: "none",
+              borderRadius: "50%",
+              width: "35px",
+              height: "35px",
+              padding: "0px",
+            }}
+          >
+            <PiRowsThin />
+          </Button>
+        </div>
         <Drawer title="Basic Drawer" onClose={onClose} open={open}>
           <p>Some contents...</p>
         </Drawer>
       </div>
-
-      {data.map((order, index) => (
-        <div
-          key={index}
-          className="flex bg-white mt-6 w-[964px] h-[150px] rounded-md"
-        >
-          <div className="flex flex-col p-4">
-            <div className="borderBottom flex items-center">
-              <Title level={5}>{order.tra_id}</Title>
+      <div className=" flex justify-center items-center content-center flex-col py-3">
+        {buyurtma.map((order, index) => (
+          <div className="flex bg-white mt-6 w-[1200px] h-[150px] rounded-md">
+            <div className="flex flex-col  px-4 py-4 justify-center gap-4 w-[200px] items-center">
+              <div className="flex gap-3">
+                <div className=" border-b-2 border-[#edeff3]">
+                  <div className=" flex justify-center items-center content-center mb-4">
+                    <Typography
+                      style={{
+                        color: "white",
+                        fontSize: "20px",
+                        backgroundColor: "#20D472",
+                        borderRadius: "30px",
+                        paddingInline: "20px",
+                        paddingBlock: "5px",
+                      }}
+                    >
+                      {order.order_id}
+                    </Typography>
+                  </div>
+                </div>
+                {/* <div className="flex justify-center items-center content-center h-[36px] w-[36px] bg-[#edeff3] rounded-full">
+                <CiBookmark style={{ fontSize: "20px", color: "#8d8e90" }} />
+              </div> */}
+              </div>
+              <div className="flex gap-3 items-center content-center">
+                <div>
+                  <MdOutlineAccessTime
+                    style={{ fontSize: "20px", color: "#8d8e90" }}
+                  />
+                </div>
+                <div>
+                  <Typography style={{ color: "#2D3A45", fontSize: "20px" }}>
+                    {order.order_time}
+                  </Typography>
+                </div>
+              </div>
             </div>
-            <div className="flex items-center">
-              <Text style={{ color: "#2D3A45" }}>{order.tra_time}</Text>
-            </div>
-          </div>
+            <div className="flex flex-col border-l-2 border-x-[#edeff3] px-4 py-3 justify-center items-center gap-6 w-[300px]">
+              <div className="flex gap-3">
+                <div>
+                  <FaRegUser
+                    style={{
+                      fontSize: "20px",
+                      color: "#8d8e90",
+                      marginTop: "9px",
+                    }}
+                  />
+                </div>
+                <div>
+                  <Typography style={{ color: "#2D3A45", fontSize: "20px" }}>
+                    {order.user_info.first_name}
+                  </Typography>
+                  <Typography style={{ color: "#2D3A45", fontSize: "20px" }}>
+                    {order.user_info.last_name}
+                  </Typography>
+                </div>
+              </div>
 
-          <div className="flex flex-col border-l-2 border-x-[#edeff3] p-4">
-            <div className="flex items-center">
-              <div className="mr-2">
-                <Avatar style={{ backgroundColor: "#87d068" }} size="large">
-                  {order.user_info.name.charAt(0)}
-                </Avatar>
+              <div className="flex gap-3">
+                <div>
+                  <FiPhone style={{ fontSize: "20px", color: "#8d8e90" }} />
+                </div>
+                <div>
+                  <Typography style={{ color: "#2D3A45", fontSize: "14px" }}>
+                    {order.user_info.phone_number}
+                  </Typography>
+                </div>
+              </div>
+            </div>
+            <div className="flex border-l-2 border-x-[#edeff3]  px-4 py-4 justify-center gap-4  w-[330px]">
+              <div className="flex flex-col gap-4">
+                <div className="flex flex-col gap-2">
+                  <div className="flex gap-2">
+                    <div>
+                      <IoClipboardOutline
+                        style={{ fontSize: "20px", color: "#8d8e90" }}
+                      />
+                    </div>
+                    <div>
+                      <Typography
+                        style={{ color: "#2D3A45", fontSize: "14px" }}
+                      >
+                        <PriceComponent
+                          price={order.order_details.order_amount}
+                        />
+                      </Typography>
+                    </div>
+                  </div>
+                  <div className="flex gap-2">
+                    <div>
+                      <CiDeliveryTruck
+                        style={{ fontSize: "20px", color: "#8d8e90" }}
+                      />
+                    </div>
+                    <div>
+                      <Typography
+                        style={{ color: "#2D3A45", fontSize: "14px" }}
+                      >
+                        <PriceComponent
+                          price={order.order_details.delivery_amount}
+                        />
+                      </Typography>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col">
+                  <Typography style={{ color: "#8d8e90", fontSize: "13px" }}>
+                    Umumiy summa
+                  </Typography>
+                  <Typography
+                    style={{
+                      color: "#2D3A45",
+                      fontSize: "20px",
+                      fontWeight: "bolder",
+                    }}
+                  >
+                    <PriceComponent price={order.order_details.total_amount} />
+                  </Typography>
+                </div>
+              </div>
+              <div className="flex">
+                {/* <span className="bg-[#14E5E4] h-[10px] w-[10px] rounded-full"></span> */}
+                {getPaymentMethod(order.order_details.payment_method)}
+              </div>
+            </div>
+            <div className="border-l-2 border-x-[#edeff3] flex flex-col  px-10 py-4 gap-6">
+              <div>
+                <Typography style={{ color: "#8d8e90", fontSize: "13px" }}>
+                  Operator:
+                </Typography>
+                <Typography style={{ color: "#2D3A45", fontWeight: "bolder" }}>
+                  Komilova M
+                </Typography>
               </div>
               <div>
-                <Text style={{ color: "#2D3A45", fontSize: "20px" }}>
-                  {order.user_info.name}
-                </Text>
+                <Typography style={{ color: "#8d8e90", fontSize: "13px" }}>
+                  Filial:
+                </Typography>
+                <Typography style={{ color: "#2D3A45", fontWeight: "bolder" }}>
+                  Max Way Maksim Gorki
+                </Typography>
               </div>
             </div>
-            <div className="flex items-center mt-2">
-              <Text style={{ color: "#2D3A45", fontSize: "14px" }}>
-                {order.user_info.phone_number}
-              </Text>
-            </div>
-          </div>
-
-          <div className="flex flex-col border-l-2 border-x-[#edeff3] p-4">
-            <div className="flex flex-col">
-              <div className="flex items-center">
-                <Text style={{ color: "#2D3A45", fontSize: "14px" }}>
-                  {order.tra_details.first_amount}
-                </Text>
+            <div className="flex flex-col relative left-[107px] gap-3 justify-center ">
+              <div className="w-[50px] h-[50px] bg-[#edeff3] rounded-full flex justify-center content-center items-center cursor-pointer">
+                <div className="w-[40px] h-[40px] bg-white rounded-full  flex justify-center content-center items-center">
+                  <TbX style={{ fontSize: "20px" }} />
+                </div>
               </div>
-              <div className="flex items-center mt-2">
-                <Text style={{ color: "#2D3A45", fontSize: "14px" }}>
-                  {order.tra_details.second_amount}
-                </Text>
-              </div>
-            </div>
-            <div className="flex items-center mt-4">
-              <Text style={{ color: "#2D3A45", fontSize: "14px" }}>
-                {order.tra_details.payment_method}
-              </Text>
-            </div>
-            <div className="flex flex-col mt-2">
-              <Text style={{ color: "#2D3A45" }}>Umumiy summa</Text>
-              <Title level={5} style={{ color: "#2D3A45" }}>
-                {order.tra_details.total_amount}
-              </Title>
-            </div>
-          </div>
-
-          <div className="flex flex-col border-l-2 border-x-[#edeff3] p-4">
-            <div className="flex items-center">
-              <Text style={{ color: "#2D3A45" }}>Operator:</Text>
-            </div>
-            <div className="flex items-center mt-2">
-              <Text style={{ color: "#2D3A45" }}>{order.operator.name}</Text>
-            </div>
-            <div className="flex items-center mt-4">
-              <Text style={{ color: "#2D3A45" }}>Filial:</Text>
-            </div>
-            <div className="flex items-center mt-2">
-              <Text style={{ color: "#2D3A45" }}>{order.branch.name}</Text>
-            </div>
-          </div>
-
-          <div className="flex flex-col relative left-[45%] gap-3 justify-center items-center">
-            <div className="w-[50px] h-[50px] bg-[#edeff3] rounded-full flex justify-center content-center items-center">
-              <div className="w-[40px] h-[40px] bg-white rounded-full flex justify-center content-center items-center">
-                <TbX style={{ fontSize: "16px" }} />
-              </div>
-            </div>
-            <div className="w-[50px] h-[50px] bg-[#edeff3] rounded-full flex justify-center content-center items-center">
-              <div className="w-[40px] h-[40px] bg-white rounded-full flex justify-center content-center items-center">
-                <IoCheckmark style={{ fontSize: "16px" }} />
+              <div className="w-[50px] h-[50px] bg-[#edeff3] rounded-full flex justify-center content-center items-center cursor-pointer">
+                <div className="w-[40px] h-[40px] bg-white rounded-full  flex justify-center content-center items-center">
+                  <IoCheckmark style={{ fontSize: "20px" }} />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
