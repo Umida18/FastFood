655dcbb3a7d9aa22fcaa443d3b1dbe76a8cf5837
umida18 <https://github.com/Navruza21/school.git>
2024-09-04 18:04:14 +0400

    U

--- src/pages/buyurtmalar/buyurmalar.tsx
+++ src/pages/buyurtmalar/buyurmalar.tsx
@@ -74,22 +74,63 @@ interface Mijoz {
   status: string;
   phone: string;
 }
+interface Branch {
+  id: number;
+  nameUz: string;
+  nameRu: string;
+  location: string;
+  operatorId: number;
+  telefon: string;
+  hours: string;
+  geometry: [];
+}
+interface Hodimlar {
+  id: number;
+  fistN: string;
+  lastN: string;
+  thName: string;
+  phone: string;
+  lavozim: string;
+  filialId: number;
+}
+interface Addres {
+  L1: number | string;
+  L2: number | string;
+}
 
-// Component
+interface Delivey {
+  id: number;
+  filialId: number;
+  narxi: number;
+  minimalNarx: string;
+}
+
 export const Buyurtmalar: React.FC = () => {
   const [open, setOpen] = useState<boolean>(false);
   const [buyurtma, setBuyurtma] = useState<Order[]>([]);
   const [tolovTuri, setTolovTuri] = useState<PaymentM[]>([]);
   const [kategoriya, setKategoriya] = useState<Category[]>([]);
-  const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<(Product & { quantity: number })[]>(
+    []
+  );
   const [mijoz, setMijoz] = useState<Mijoz[]>([]);
   const [activeTab, setActiveTab] = useState<string>("yangi");
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [selectedView, setSelectedView] = useState<string>("columns");
   const [openSelMijoz, setOpenSelMijoz] = useState<boolean>(false);
-  const [addProdList, setAddProdList] = useState<Product[]>([]);
+  const [addProdList, setAddProdList] = useState<
+    (Product & { quantity: number })[]
+  >([]);
   const [selectedClient, setSelectedClient] = useState<Mijoz | null>(null);
-  const [address, setAddress] = useState<string>("");
+  const [address, setAddress] = useState<Addres[]>([]);
+  const [filial, setFilial] = useState<Branch[]>([]);
+  const [hodim, setHodim] = useState<Hodimlar[]>([]);
+  const [selectedFilial, setSelectedFilial] = useState<Branch | null>(null);
+  const [selectedOperator, setSelectedOperator] = useState<Hodimlar | null>(
+    null
+  );
+  const [delivery, setDelivery] = useState<Delivey[]>([]);
+  const [delNarx, setDelNarx] = useState<number>();
 
   const showDrawer = () => {
     setOpen(true);
@@ -116,11 +157,23 @@ export const Buyurtmalar: React.FC = () => {
       const responseMijoz = await axios.get(
         "https://10d4bfbc5e3cc2dc.mokky.dev/mijoz"
       );
+      const responseF = await axios.get(
+        "https://3c2999041095f9d9.mokky.dev/filial"
+      );
+      const responseH = await axios.get(
+        "https://10d4bfbc5e3cc2dc.mokky.dev/Hodimlar"
+      );
+      const responseDelivery = await axios.get(
+        "https://3c2999041095f9d9.mokky.dev/delivery"
+      );
       setProducts(responseProducts.data);
       setBuyurtma(responseB.data);
       setTolovTuri(responseT.data);
       setKategoriya(responseCat.data);
       setMijoz(responseMijoz.data);
+      setFilial(responseF.data);
+      setHodim(responseH.data);
+      setDelivery(responseDelivery.data);
     };
     fetchData();
   }, []);
@@ -207,8 +260,41 @@ export const Buyurtmalar: React.FC = () => {
   };
 
   const addProductToList = (product: Product) => {
-    setAddProdList((prev) => [...prev, product]);
+    setAddProdList((prev) => {
+      const existingProduct = prev.find((p) => p.id === product.id);
+      if (existingProduct) {
+        return prev.map((p) =>
+          p.id === product.id ? { ...p, quantity: p.quantity + 1 } : p
+        );
+      } else {
+        return [...prev, { ...product, quantity: 1 }];
+      }
+    });
   };
+
+  const incrementQuantity = (productId: number) => {
+    setAddProdList((prev) =>
+      prev.map((p) =>
+        p.id === productId ? { ...p, quantity: p.quantity + 1 } : p
+      )
+    );
+  };
+
+  const decrementQuantity = (productId: number) => {
+    setAddProdList((prev) =>
+      prev
+        .map((p) =>
+          p.id === productId ? { ...p, quantity: p.quantity - 1 } : p
+        )
+        .filter((p) => p.quantity > 0)
+    );
+  };
+  const [isProductAdded, setIsProductAdded] = useState(false);
+
+  // const handleAddProduct = () => {
+  //   addProductToList(item);
+  //   setIsProductAdded(true);
+  // };
 
   const totalPrice = addProdList.reduce((total, prod) => {
     const piceT = prod.price.toString();
@@ -217,14 +303,26 @@ export const Buyurtmalar: React.FC = () => {
   const delListProd = () => {
     setAddProdList([]);
   };
-  const handleClientChange = (clientId: number) => {
-    const client = mijoz.find((m) => m.id === clientId);
+
+  const handleClientChange = (Id: number) => {
+    const client = mijoz.find((m) => m.id === Id);
     setSelectedClient(client ? client : null);
+  };
+
+  const handleBranchh = (id: number) => {
+    const bran = filial.find((f) => f.id === id);
+    setSelectedFilial(bran ? bran : null);
+
+    if (bran) {
+      const operator = hodim.find((h) => h.id === bran.operatorId);
+      setSelectedOperator(operator ? operator : null);
+    }
   };
 
   const handleMapClick = (e: any) => {
     const coords = e.get("coords");
-    setAddress(`Latitude: ${coords[0]}, Longitude: ${coords[1]}`); // You might need to format this based on your needs
+    setAddress([{ L1: coords[0], L2: coords[1] }]);
+    console.log(address);
   };
 
   const handleSubmit = async () => {
@@ -251,40 +349,75 @@ export const Buyurtmalar: React.FC = () => {
         total_amount: totalPrice.toString(),
         payment_method: 1,
       },
-      filial_id: 1,
+      filial_id: selectedFilial?.id,
       mijoz_id: selectedClient.id,
     };
 
     try {
-      // POST request to create a new order
       const resp = await axios.post(
         "https://10d4bfbc5e3cc2dc.mokky.dev/buyurtma",
         orderData
       );
 
-      // Extract the new order's ID from the response
       const newOrderId = resp.data.id;
 
-      // Update the orderData with the new order_id
       orderData.order_id = newOrderId;
 
-      // PATCH request to update the newly created order with the correct order_id
       await axios.patch(
         `https://10d4bfbc5e3cc2dc.mokky.dev/buyurtma/${newOrderId}`,
         { order_id: newOrderId }
       );
 
-      // Update the UI with the new order
       setBuyurtma((prevOrders) => [...prevOrders, resp.data]);
 
-      // Optionally reset the form
       setAddProdList([]);
       setSelectedClient(null);
-      setAddress("");
+      setSelectedFilial(null);
+      setAddress([]);
       setOpen(false);
     } catch (error) {
       console.log("Error submitting order:", error);
     }
+  };
+  const getFilial = (id: number) => {
+    const fil = filial.find((f) => f.id === id);
+    return fil ? fil.nameUz : "-";
+  };
+  const getHodim = (filialId: number) => {
+    // const filiaL = filial.find((f) => f.id === filialId);
+
+    if (selectedFilial) {
+      const employee = hodim.find((h) => h.id === selectedFilial.operatorId);
+      console.log(employee);
+
+      return employee
+        ? `${employee.fistN} ${employee.lastN} ${employee.thName}`
+        : "Hodim topilmadi";
+    }
+
+    return "filial topilmadi";
+  };
+  const getOperatorForOrder = (filialId: number) => {
+    const bran = filial.find((f) => f.id === filialId);
+
+    if (bran) {
+      const operator = hodim.find((h) => h.id === bran.operatorId);
+      return operator
+        ? `${operator.fistN} ${operator.lastN} ${operator.thName}`
+        : "Operator topilmadi";
+    }
+
+    return "Filial topilmadi";
+  };
+
+  const narxDelivery = (id: number): string => {
+    const branch = filial.find((f) => f.id === id);
+    if (branch) {
+      const deliveryI = delivery.find((d) => d.filialId === branch.id);
+      // setDelNarx(deliveryI?.narxi)
+      return deliveryI ? deliveryI.narxi.toString() : "-";
+    }
+    return "N/A";
   };
   return (
     <div className="">
@@ -404,17 +537,39 @@ export const Buyurtmalar: React.FC = () => {
                                   <div>
                                     <PriceComponent price={item.price} />
                                   </div>
-                                  <Button
-                                    style={{
-                                      backgroundColor: "#20D472",
-                                      color: "white",
-                                      border: "none",
-                                    }}
-                                    icon={<GoPlus />}
-                                    onClick={() => addProductToList(item)}
-                                  >
-                                    Qo'shish
-                                  </Button>
+                                  <div>
+                                    {isProductAdded ? (
+                                      <div className="flex gap-2">
+                                        <Button
+                                          onClick={() =>
+                                            decrementQuantity(item.id)
+                                          }
+                                        >
+                                          -
+                                        </Button>
+                                        <Typography>{item.quantity}</Typography>
+                                        <Button
+                                          onClick={() =>
+                                            incrementQuantity(item.id)
+                                          }
+                                        >
+                                          +
+                                        </Button>
+                                      </div>
+                                    ) : (
+                                      <Button
+                                        style={{
+                                          backgroundColor: "#20D472",
+                                          color: "white",
+                                          border: "none",
+                                        }}
+                                        icon={<GoPlus />}
+                                        onClick={() => addProductToList(item)}
+                                      >
+                                        Qo'shish
+                                      </Button>
+                                    )}
+                                  </div>
                                 </div>
                               </div>
                             </div>
@@ -457,7 +612,7 @@ export const Buyurtmalar: React.FC = () => {
                   {/* order list */}
                   <div className="orderList">
                     {addProdList.map((item, index) => (
-                      <div key={index} className="flex">
+                      <div key={index} className="flex justify-between">
                         <div>
                           <p>{item.name}</p>
                         </div>
@@ -499,10 +654,46 @@ export const Buyurtmalar: React.FC = () => {
                     />
                   </Form.Item>
                   <Form.Item>
+                    <Typography>Filial</Typography>
+                    <Select
+                      style={{ width: "330px" }}
+                      // suffixIcon={<FiUserPlus />}
+                      onChange={handleBranchh}
+                    >
+                      {filial.map((item) => (
+                        <Select.Option key={item.id} value={item.id}>
+                          {item.nameUz}
+                        </Select.Option>
+                      ))}
+                    </Select>
+                  </Form.Item>
+                  <Form.Item>
+                    <Typography>Operator</Typography>
+                    <Input
+                      readOnly
+                      value={
+                        selectedOperator
+                          ? `${selectedOperator.fistN} ${selectedOperator.lastN} ${selectedOperator.thName}`
+                          : "Operator tanlanmadi"
+                      }
+                    />
+                  </Form.Item>
+                  <Form.Item>
                     <Typography>Manzil</Typography>
                     <Input
-                      value={address}
-                      onChange={(e) => setAddress(e.target.value)}
+                      value={
+                        address.length
+                          ? `L1: ${address[0].L1}, L2: ${address[0].L2}`
+                          : ""
+                      }
+                      onChange={(e) =>
+                        setAddress([
+                          {
+                            L1: e.target.value.split(",")[0],
+                            L2: e.target.value.split(",")[1],
+                          },
+                        ])
+                      }
                     />
                     <YMaps
                       query={{
@@ -516,7 +707,14 @@ export const Buyurtmalar: React.FC = () => {
                         }}
                         onClick={handleMapClick}
                         modules={["geocode"]}
-                      />
+                      >
+                        {address.map((item: any, index: any) => (
+                          <Placemark
+                            key={index}
+                            geometry={[item.L1, item.L2]}
+                          />
+                        ))}
+                      </Map>
                     </YMaps>
                   </Form.Item>
                   <Form.Item>
@@ -628,9 +826,7 @@ export const Buyurtmalar: React.FC = () => {
                         <Typography
                           style={{ color: "#2D3A45", fontSize: "14px" }}
                         >
-                          <PriceComponent
-                            price={order.order_details.delivery_amount}
-                          />
+                          {narxDelivery(order.filial_id)}
                         </Typography>
                       </div>
                     </div>
@@ -664,7 +860,7 @@ export const Buyurtmalar: React.FC = () => {
                   <Typography
                     style={{ color: "#2D3A45", fontWeight: "bolder" }}
                   >
-                    Komilova M
+                    {getOperatorForOrder(order.filial_id)}
                   </Typography>
                 </div>
                 <div>
@@ -674,7 +870,7 @@ export const Buyurtmalar: React.FC = () => {
                   <Typography
                     style={{ color: "#2D3A45", fontWeight: "bolder" }}
                   >
-                    Max Way Maksim Gorki
+                    {getFilial(order.filial_id)}
                   </Typography>
                 </div>
               </div>
